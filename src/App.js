395ec61ce8fb3c9386dc601aa933af9c78673000
karmit1296 <karmit1lalani@gmail.com>
2022-03-08 00:16:37 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const url= `https://api.adviceslip.com/advice`;
+
+  const[quote, setQuote] = useState('Just do it!');
+
+  useEffect(()=>{
+    getQuote();
+  }, [])
+
+  const getQuote = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setQuote(data.slip.advice);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container">
+          <p>{quote}</p>
+          <button onClick={getQuote}>Get Quote</button>
+          </div>
     </div>
   );
 }
